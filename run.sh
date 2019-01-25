ccc16512ee66f4900fd4b865f5ea96e84f8831c0
moratorium08 <moratorium08@gmail.com>
2019-01-25 07:39:58 +0000

    Add runner

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# できればcargoで完結させたいが、一旦めんどくさいので
+env CC=riscv32-unknown-linux-gnu-gcc cargo xbuild --target riscv32ima-unknown-none-elf.json
+tools/bin/simu target/riscv32ima-unknown-none-elf/debug/osmium.bin a
