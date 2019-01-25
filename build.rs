9b82ef72efe4a0044bb6128a2545c1055652aae3
moratorium08 <moratorium08@gmail.com>
2019-01-25 07:53:15 +0000

    Add setup/build script

--- build.rs
+++ build.rs
@@ -1,14 +1,25 @@
 extern crate cc;
 
-use std::error::Error;
 use cc::Build;
+use std::env;
+use std::error::Error;
+use std::path::Path;
+use std::process::Command;
 
 fn main() -> Result<(), Box<Error>> {
-    // assemble the `asm.s` file
+
     Build::new()
         .file("boot.s")
         //.flag("-mabi=ilp32")
         .compile("asm");
+
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let input_path = Path::new(&out_dir).join("../../../osmium");
+    let output_path = Path::new(&out_dir).join("../../../osmium.bin");
+
+    Command::new("tools/bin/elf2bin")
+        .args(&[input_path.to_str().unwrap(), output_path.to_str().unwrap()])
+        .status()?;
 
     Ok(())
 }
--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+rm -rf tools
 git clone --recursive https://github.com/cpu-3/tools
 make build -C tools
