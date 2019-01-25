b39266cd3850f257ca9a96d443d6e653023d2aa5
moratorium08 <moratorium08@gmail.com>
2019-01-25 11:10:24 +0000

    fix sp

--- boot.s
+++ boot.s
@@ -5,10 +5,10 @@
 
 _start:
     /* Set up stack pointer. */
-    lui     sp, %hi(stacks + 1024)
-    ori     sp, sp, %lo(stacks + 1024)
+    li     sp, 0xf4230
     /* Now jump to the rust world; __start_rust.  */
     j       __start_rust
+
 
 .bss
 
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # できればcargoで完結させたいが、一旦めんどくさいので
-env CC=riscv32-unknown-linux-gnu-gcc cargo xbuild --target riscv32ima-unknown-none-elf.json
+env CC=riscv32-unknown-linux-gnu-gcc cargo xbuild --target riscv32ima-unknown-none-elf.json -vv
 tools/bin/simu target/riscv32ima-unknown-none-elf/debug/osmium.bin a
