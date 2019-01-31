023316ed81fa73b8582e9b66847c962dd02d6016
moratorium08 <moratorium08@gmail.com>
2019-01-31 13:47:58 +0900

    Fix SATP handling

--- src/csr.rs
+++ src/csr.rs
@@ -11,84 +11,80 @@ fn read(addr: u16) -> u32 {
     result
 }
 
-trait CSR {
-    fn to_u32(&self) -> u32;
-    fn addr(&self) -> u16;
-    fn update(&mut self, val: u32);
+fn read_and_write(addr: u16, val: u32) -> u32 {
+    let result: u32;
 
-    fn read_and_write(&mut self) {
-        let addr = self.addr();
-        let val = self.to_u32();
-        let result: u32;
+    unsafe {
+        asm!("csrrs $0, $1, $2"
+            : "=&r"(result)
+            : "r"(addr),
+                "r"(val));
+    }
+    result
+}
 
-        unsafe {
-            asm!("csrrs $0, $1, $2"
-                : "=&r"(result)
-                : "r"(addr),
-                  "r"(val));
-        }
+fn write(addr: u16, val: u32) {
+    unsafe {
+        asm!("csrrw x0, $0, $1"
+            :
+            : "r"(addr),
+              "r"(val));
+    }
+}
+
+fn bit_set(addr: u16, bitvec: u32) {
+    unsafe {
+        asm!("csrrw x0, $0, $1"
+            :
+            : "r"(addr),
+              "r"(bitvec));
+    }
+}
+
+fn bit_clear(addr: u16, bitvec: u32) {
+    unsafe {
+        asm!("csrrc x0, $0, $1"
+            :
+            : "r"(addr),
+              "r"(bitvec));
     }
 
-    fn write(&self) {
-        let result: u32;
-        unsafe {
-            asm!("csrrw x0, $0, $1"
-                :
-                : "r"(self.addr()),
-                  "r"(self.to_u32()));
-        }
-    }
-
-    fn bit_set(&self, bitvec: u32) {
-        unsafe {
-            asm!("csrrw x0, $0, $1"
-                :
-                : "r"(self.addr()),
-                  "r"(bitvec));
-        }
-    }
-
-    fn bit_clear(&self, bitvec: u32) {
-        unsafe {
-            asm!("csrrc x0, $0, $1"
-                :
-                : "r"(self.addr()),
-                  "r"(bitvec));
-        }
-
-    }
 }
 
 const SATP_ADDR: u16 = 0x180;
 struct SATP {
-    paging_on: bool,
-    ppn: u32
+    pub paging_on: bool,
+    pub ppn: u32
 }
 
 impl SATP {
-    fn new(ppn: u32, paging_on: bool) -> SATP {
-        SATP{ppn, paging_on}
+    fn to_u32(&self) -> u32 {
+        let paging_on = if self.paging_on {1} else {0};
+        (paging_on << 31) | self.ppn
     }
     fn read() -> SATP {
         let v = read(SATP_ADDR);
         SATP{paging_on: utils::bit_range(v, 31, 32) == 1,
              ppn: utils::bit_range(v, 0, 22)}
     }
-}
-
-
-impl CSR for SATP {
-    fn to_u32(&self) -> u32 {
-        let paging_on = if self.paging_on {1} else {0};
-        (paging_on << 31) | self.ppn
+    fn commit(&self) {
+        let v = self.to_u32();
+        write(SATP_ADDR, v);
     }
-    
-    fn addr(&self) -> u16 {
-        SATP_ADDR
+    fn write(paging_on: bool, ppn: u32) {
+        SATP{paging_on, ppn}.commit();
     }
-
-    fn update(&mut self, val: u32) {
-        self.paging_on = utils::bit_range(val, 31, 32) == 1;
-        self.ppn = utils::bit_range(val, 0, 22);
+    fn enable_paging() {
+        let v = SATP{paging_on: true, ppn: 0}.to_u32();
+        bit_set(SATP_ADDR, v);
+    }
+    fn disable_paging() {
+        let v = SATP{paging_on: true, ppn: 0}.to_u32();
+        bit_clear(SATP_ADDR, v);
+    }
+    fn set_ppn(ppn: u32) {
+        let mut satp = SATP::read();
+        satp.ppn = ppn;
+        satp.commit();
     }
 }
