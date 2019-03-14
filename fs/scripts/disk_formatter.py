6973a073cc805872a69ddbc60323a9e397c5115f
moratorium08 <moratorium08@gmail.com>
2019-03-15 00:08:16 +0900

    Add File

--- fs/scripts/disk_formatter.py
+++ fs/scripts/disk_formatter.py
@@ -1,30 +1,51 @@
+#!/usr/bin/env python3
 import os
 import argparse
 import struct
 import logging
 
+N_BLOCKS = 20  # 00
+BLOCK_SIZE = 4096
+
+
 def p32(val):
-    struct.pack('<I', val)
+    return struct.pack('<I', val)
+
 
 def p16(val):
-    struct.pack('<H', val)
+    return struct.pack('<H', val)
+
 
 def p8(val):
-    struct.pack('<B', val)
+    return struct.pack('<B', val)
+
 
 def st_mode2mode(m):
     return 0
+
+
+def read_exact(f, size):
+    cur = 0
+    s = b''
+    while True:
+        tmp = f.read(size - cur)
+        cur += len(tmp)
+        if len(tmp) == 0:
+            break
+        s += tmp
+        if cur == size:
+            break
+    return s
+
 
 # type
 DIRECTORY_TYPE = 1
 FILE_TYPE = 2
 
+
 class Data:
     def write(self, image):
         raise NotImplemented
-
-class SuperBlock(Block):
-    pass
 
 
 '''
@@ -44,7 +65,9 @@ direcotry cannot hold more than 957 * 1024 files
 '''
 
 DIRECTORY_FILE_MAX = 957 * (BLOCK_SIZE // 4)
-class Directory(Block):
+
+
+class Directory(Data):
     def __init__(self, path):
         self.name = os.path.basename(path)
         # should implement
@@ -71,15 +94,15 @@ class Directory(Block):
         files = os.listdir(self.path)
         if len(files) > DIRECTORY_FILE_MAX:
             raise Exception('{} holds too much files: {} > {}',
-                    self.path,
-                    len(files),
-                    DIRECTORY_FILE_MAX)
+                            self.path,
+                            len(files),
+                            DIRECTORY_FILE_MAX)
         indices = []
         for name in files:
             p = os.path.join(self.path, name)
             data = data_factory(p)
             if data is not None:
-                indices.append(data.write(p))
+                indices.append(data.write(image))
 
         # 2. write the indirect indices to image and save these indices
         indirect_blocks = []
@@ -99,9 +122,77 @@ class Directory(Block):
         self.indirect_blocks = indirect_blocks
         return self._write(image)
 
-class File(Block):
+
+class File(Data):
     def __init__(self, path):
         self.name = os.path.basename(path)
+        # should implement
+        self.permission = 0
+        self.owner = 0
+        self.group = 0
+        self.path = path
+
+    def _write(self, image):
+        block = b''
+        block += p8(FILE_TYPE)
+        block += p8(0)
+        block += self.name.encode('utf-8')
+        block += p16(self.permission)
+        block += p16(self.owner)
+        block += p16(self.group)
+        block += p32(self.size)
+        for index in self.indirect_blocks:
+            block += p32(index)
+        return image.write_block(block)
+
+    def write(self, image):
+        with open(self.path, "rb") as f:
+            size = 0
+            blocks = []
+            while True:
+                s = read_exact(f, BLOCK_SIZE)
+                block = image.write_block(s)
+                blocks.append(block)
+                size += len(s)
+                if len(s) < BLOCK_SIZE:
+                    break
+
+            indirect_blocks1 = []
+            indirect_blocks2 = b''
+            direct_block = b''
+            for block in blocks:
+                if len(indirect_blocks2) == BLOCK_SIZE:
+                    b = image.write_block(indirect_blocks2)
+                    indirect_blocks1.append(b)
+                    indirect_blocks2 = b''
+                if len(direct_block) == BLOCK_SIZE:
+                    b = image.write_block(direct_block)
+                    indirect_blocks2 += p32(b)
+                    direct_block = b''
+                direct_block += p32(block)
+
+            if len(indirect_blocks2) == BLOCK_SIZE:
+                b = image.write_block(indirect_blocks2)
+                indirect_blocks1.append(b)
+                indirect_blocks2 = b''
+            if len(direct_block) == BLOCK_SIZE:
+                b = image.write_block(direct_block)
+                indirect_blocks2 += p32(b)
+                direct_block = b''
+            if len(indirect_blocks2) == BLOCK_SIZE:
+                b = image.write_block(indirect_blocks2)
+                indirect_blocks1.append(b)
+                indirect_blocks2 = b''
+            if len(direct_block) != 0:
+                b = image.write_block(direct_block)
+                indirect_blocks2 += p32(b)
+            if len(indirect_blocks2) != 0:
+                b = image.write_block(indirect_blocks2)
+                indirect_blocks1.append(b)
+
+            self.indirect_blocks = indirect_blocks1
+            self.size = size
+        return self._write(image)
 
 
 def data_factory(path):
@@ -110,8 +201,11 @@ def data_factory(path):
     elif os.path.isdir(path):
         return Directory(path)
     else:
-        logging.warning('%s is ignored because this is not a file nor a directory', path)
+        logging.warning(
+            '%s is ignored because this is not a file nor a directory', path)
         return None
+
+
 '''
 --------------
 | SuperBlock |
@@ -133,20 +227,32 @@ Block: File/Directory data
 * filename convention: must end with '\x00'
 '''
 
-N_BLOCKS = 100000
-BLOCK_SIZE = 4096
+
+def fill_zero(f, size):
+    # batch by BLOCK_SIZE
+    s = b'\x00' * BLOCK_SIZE
+    for i in range(size // BLOCK_SIZE):
+        f.write(s)
+    f.write(b'\x00' * (size % BLOCK_SIZE))
+    f.seek(0)
+
+
 class Image:
-    def __init__(root, disk, n_blocks=N_BLOCKS):
+
+    def __init__(self, root, disk, n_blocks=N_BLOCKS):
         if type(root) != Directory:
             raise TypeError("root must be a directory")
         self.root = root
-        self.disk = open(disk, 'rb+')
+        self.disk = open(disk, 'wb+')
         self.n_blocks = n_blocks
-
         padding = (N_BLOCKS % (BLOCK_SIZE * 8)) != 0
-        blocks_base_index = N_BLOCKS // (BLOCK_SIZE * 8) + padding + 1 # super block
+        # super block
+        blocks_base_index = N_BLOCKS // (BLOCK_SIZE * 8) + padding + 1
         # free area stack. for debugging simplicity, use from smaller index
         self.management = list(range(N_BLOCKS - 1, blocks_base_index - 1, -1))
+
+        # first initialize disk file
+        fill_zero(self.disk, (blocks_base_index + N_BLOCKS) * BLOCK_SIZE)
 
     def _alloc_block(self):
         return self.management.pop()
@@ -159,7 +265,7 @@ class Image:
         self.disk.seek(place)
         self.disk.write(data)
 
-    def write_block(data):
+    def write_block(self, data):
         '''
         write block to disk
 
@@ -173,7 +279,8 @@ class Image:
           where the data has been written
         '''
         if len(data) > BLOCK_SIZE:
-            raise Exception('block size is too large: {} > {}'.format(len(data), BLOCK_SIZE))
+            raise Exception('block size is too large: {} > {}'.format(
+                len(data), BLOCK_SIZE))
 
         index = self._alloc_block()
         self._write_block_at(data, index)
@@ -188,17 +295,23 @@ class Image:
 
     def _finalize(self):
         self._write_management()
-        self._write__super_block()
+        self._write_super_block()
 
-    def generate(self, outchan):
+    def generate(self):
         self.root.write(self)
         self._finalize()
 
+
 def main():
-    argparse.ArgumentParser(description='instant disk image formatter')
-    parser.add_argument('dir', help='source directory')
+    parser = argparse.ArgumentParser(
+        description='instant disk image formatter')
+    parser.add_argument('root', help='source directory')
     parser.add_argument('image', help='image filename')
     args = parser.parse_args()
+
+    root = Directory(args.root)
+    image = Image(root, args.image)
+    image.generate()
 
 
 if __name__ == '__main__':
